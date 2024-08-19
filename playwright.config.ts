56e58cf8f800f36e4957ef6d7e433841c14b96eb
Lakshani-P <devdun90@gmail.com>
2024-08-19 09:23:20 +0530

    playwrite.config file

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  timeout: 30000,
+  retries: 2,
+  testDir: './tests',
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    baseURL: 'https://www.saucedemo.com',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'webkit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+  outputDir: 'test-results/',
+  reporter: [['html', { open: 'never' }]],
+};
+
+export default config;
